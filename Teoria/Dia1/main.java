007506fd91438a87e13c7e71867c2cf3ea9e48b5
SagittariusITG <joel.arnaud@itgranada.com>
2022-04-20 14:23:33 +0200

    Dia 1

--- Teoria/Dia1/main.java
+++ Teoria/Dia1/main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class main{
     public static void main(String[] args){
         System.out.println("asd");
+
+        // casting manual
+        int letra = 65;
+        char caracter = (char)letra;
+        System.out.println(caracter);
     }
 }
