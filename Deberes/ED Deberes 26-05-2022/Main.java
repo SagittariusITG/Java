992a43d4b46dc277fb5409b1e616f5cf60b8fefb
SagittariusITG <joel.arnaud@itgranada.com>
2022-04-26 19:00:34 +0200

    Deberes ED

---
+++ Deberes/ED Deberes 26-05-2022/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args){
+      Electrodomestico miElectrodomestico = new Electrodomestico(120,"miColor","miConsumo",120);
+      System.out.println(miElectrodomestico);
+    }
+
+    // ¿Que hacer en la bateria de pruebas?
+    // Comprobar que todos los construcores deseados funcionen
+    // Comprobar que todos los gets y los sets funcionen.
+    // Comprobar que el precio final funcione correctamente.
+}
